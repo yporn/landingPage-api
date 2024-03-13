63eb5c3f9e87e8d6a9a11c1c29690363bbd631b6
w3-devmaster <apichat_pep@hotmail.com>
2024-03-13 12:17:27 +0700

    update

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# /go/bin/
-migrate -database 'postgres://admin:password@db:5432/sirarom_db?sslmode=disable' -source file://pkg/databases/migrations -verbose up
-
-# Start the main application
-./main
